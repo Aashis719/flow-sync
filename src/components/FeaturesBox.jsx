1363eda8ac2231026c8efc52309b4a3671fb8c9a
Aashish Neupane <162073333+Aashis719@users.noreply.github.com>
2025-09-10 21:31:07 +0545

    Update FeaturesBox.jsx

--- src/components/FeaturesBox.jsx
+++ src/components/FeaturesBox.jsx
@@ -7,7 +7,7 @@ const FeaturesBox = ({ icon:Icon, title, description, link }) => {
       <div className='border-l-3 border-purple-400 p-5 pt-5 w-80 mt-10 rounded-xl flex flex-col gap-2 feature-box hover:scale-103 flex-wrap transition-all duration-200 ease-in-out shadow-md shadow-gray-200'>
         <span className='text-purple-500 text-5xl icon'>
           <Icon />
-        </span>
+        </span> 
         <h1 className='text-2xl font-semibold'>{title}</h1>
         <p>{description}</p>
         <a href={link} className='flex items-center gap-2 mt-3 text-blue-500'>
